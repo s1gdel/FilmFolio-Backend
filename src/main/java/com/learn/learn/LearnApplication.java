fb52d98d6689309c6aee1c711257d8392f54cad6
s1gdel <subhamsigdel@gmail.com>
2025-01-12 20:18:04 -0600

    FilmFolio

--- src/main/java/com/learn/learn/LearnApplication.java
+++ src/main/java/com/learn/learn/LearnApplication.java
@@ -1,5 +1,6 @@
 package com.learn.learn;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearnApplication {
 
     public static void main(String[] args) {
+        // Load environment variables from .env file
+        Dotenv dotenv = Dotenv.configure().load();
+
+        // Set the environment variables to System properties
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(LearnApplication.class, args);
     }
-
 }
